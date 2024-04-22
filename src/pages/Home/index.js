3ffdb65400f3c60d0936b39e072ecd676148292c
Hugo Paynaud <hugopaynaud4@gmail.com>
2024-04-22 19:34:24 +0200

    correcting derniere prestation in footer and adding div for nav links

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,14 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const {last} = useData()
+  const { data } = useData();
+  console.log("Data:", data); 
+
+  const events = data && data.events ? data.events : [];
+  console.log("Events:", events); 
+
+  const lastEvent = events.length > 0 ? events[events.length - 1] : null;
+  console.log("Last Event:", lastEvent); 
   return <>
     <header>
       <Menu />
@@ -22,7 +29,7 @@ const Page = () => {
       <section className="SliderContainer">
         <Slider />
       </section>
-      <section className="ServicesContainer">
+      <section className="ServicesContainer" id="nos-services">
         <h2 className="Title">Nos services</h2>
         <p>Nous organisons des événements sur mesure partout dans le monde</p>
         <div className="ListContainer">
@@ -51,11 +58,11 @@ const Page = () => {
           </ServiceCard>
         </div>
       </section>
-      <section className="EventsContainer">
+      <section className="EventsContainer" id="nos-realisations">
         <h2 className="Title">Nos réalisations</h2>
         <EventList />
       </section>
-      <section className="PeoplesContainer">
+      <section className="PeoplesContainer" id="notre-equipe">
         <h2 className="Title">Notre équipe</h2>
         <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
         <div className="ListContainer">
@@ -114,16 +121,20 @@ const Page = () => {
       </div>
     </main>
     <footer className="row">
-      <div className="col presta">
-        <h3>Notre derniére prestation</h3>
-        <EventCard
-          imageSrc={last?.cover}
-          title={last?.title}
-          date={new Date(last?.date)}
-          small
-          label="boom"
-        />
-      </div>
+    <div className="col presta">
+          <h3>Notre dernière prestation</h3>
+          {lastEvent ? (
+            <EventCard
+              imageSrc={lastEvent.cover}
+              title={lastEvent.title}
+              date={new Date(lastEvent.date)}
+              small
+              label="boom"
+            />
+          ) : (
+            <p>Aucune dernière prestation trouvée</p>
+          )}
+        </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
         <address>45 avenue de la République, 75000 Paris</address>
